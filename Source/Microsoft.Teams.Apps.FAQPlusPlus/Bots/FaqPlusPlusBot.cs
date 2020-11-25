20e5ad745c15e54fc958d5e497d95da9e7bcd66f
alexAlvarez16 <reyes.alvarez@xpertal.com>
2020-11-24 18:14:38 -0600

    Revert "1"
    This reverts commit 707d3cf590966a0006a0595aa2d6b49cf53da536.

--- Source/Microsoft.Teams.Apps.FAQPlusPlus/Bots/FaqPlusPlusBot.cs
+++ Source/Microsoft.Teams.Apps.FAQPlusPlus/Bots/FaqPlusPlusBot.cs
@@ -6,14 +6,11 @@ namespace Microsoft.Teams.Apps.FAQPlusPlus.Bots
 {
     using System;
     using System.Collections.Generic;
-    using System.Data;
     using System.Globalization;
-    using System.IO;
     using System.Linq;
     using System.Net;
     using System.Threading;
     using System.Threading.Tasks;
-    using ChoETL;
     using Microsoft.ApplicationInsights.DataContracts;
     using Microsoft.Azure.CognitiveServices.Knowledge.QnAMaker.Models;
     using Microsoft.Bot.Builder;
