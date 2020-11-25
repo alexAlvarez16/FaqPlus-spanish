707d3cf590966a0006a0595aa2d6b49cf53da536
alexAlvarez16 <reyes.alvarez@xpertal.com>
2020-11-24 18:00:27 -0600

    1

---
+++ Source/Microsoft.Teams.Apps.FAQPlusPlus/Profiling.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microsoft.Teams.Apps.FAQPlusPlus
+{
+    public class Profiling
+    {
+        public string nombreCompleto { get; set; }
+        public string email { get; set; }
+        public string division { get; set; }
+        public string area_personal { get; set; }
+        public string perfil { get; set; }
+        public string ubicacion { get; set; }
+        public string aguinaldo { get; set; }
+        public string prima_vacacional { get; set; }
+        public string cac { get; set; }
+        public string clinica_scyf { get; set; }
+        public string fea_10 { get; set; }
+        public string fea_13 { get; set; }
+        public string caja_ahorro { get; set; }
+        public string fondo_ayuda_mutua { get; set; }
+        public string seguro_vida { get; set; }
+        public string plan_cultivemos { get; set; }
+        public string seguros_axa_poliza_ejecutivos { get; set; }
+        public string seguros_axa_poliza_basica { get; set; }
+        public string seguros_medikplus_extension_suma_asegurada { get; set; }
+        public string seguros_medikplus_foraneos { get; set; }
+        public string seguros_medikplus_reembolso { get; set; }
+        public string gasolina_ticket_car_1530 { get; set; }
+        public string gasolina_ticket_car_3060 { get; set; }
+        public string gasolina_oxxo_Gas_1530 { get; set; }
+        public string gasolina_oxxo_Gas_3060 { get; set; }
+        public string vale_despensa_7 { get; set; }
+        public string vale_despensa_10 { get; set; }
+        public string prestamo_personal { get; set; }
+        public string prestamo_hipotecario { get; set; }
+        public string seguro_cancer { get; set; }
+        public string seguro_casa { get; set; }
+        public string seguro_auto { get; set; }
+        public string seguro_viajes { get; set; }
+        public string médicos_menores { get; set; }
+        public string Descuentos_autos { get; set; }
+        public string descuentos_academicos { get; set; }
+        public string descuentos_laboratorios { get; set; }
+        public string descuentos_opticas { get; set; }
+        public string bono_auto { get; set; }
+        public string dote_matrimonial { get; set; }
+        public string apoyo_cerveza_Aniversarios_XV { get; set; }
+        public string juguetes_scyf { get; set; }
+        public string rifa_auto_scyf { get; set; }
+
+    }
+}
